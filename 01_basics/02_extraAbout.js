dfb94ab5329c176c5ebbf3c88e7fcdd408b58822
NEGO2522 <negokshitij@gmail.com>
2025-03-22 15:07:40 +0000

    Datatypes

---
+++ 01_basics/02_extraAbout.js
@@ -0,0 +1,8 @@
+"use strict"; // Treat all js as new version of javascript.
+
+// alert("Hello, World!"); // This command show an error because we are using node js not browser.
+ console.log(3+3); // code readability should be high.
+
+ console.log("Kshitij");
+
+ // To see More About Alert We can Use Mdn Refrences alert docs.
---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,23 @@
+const name = "Kshitij" //  string datatype.
+let age = 18 // Numbers Datatype.
+let isTopper = false // Boolen Datatype.
+let state;
+
+// Number => 2 
+// bigint
+// string => ""
+// boolean => True or False
+// null => standalone value
+// Undefined => when u don't define a value
+// symbol => unique
+
+
+// object => This is non primitive datatype.
+
+
+console.log(typeof "Kshitij");
+console.log(typeof "");
+console.log(typeof 18);
+console.log(typeof false);
+console.log(typeof  null); // output :- Object
+console.log(typeof undefined); // Output :- undefined
